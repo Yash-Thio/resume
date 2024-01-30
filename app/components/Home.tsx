00f8c5b0bb7cf73b220e245dcf421deae76d2e86
Yash Pandey <pandeyash06@gmail.com>
2024-01-30 23:56:06 +0530

    changed hero component style

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,9 +1,9 @@
 export default function Home(){
     return(
     <div className="home space-y-4" id="home">
-        <h1 className="text-7xl font-apple-system">Hello, I&#39;m</h1>
-        <h1 className="text-8xl font-apple-system"><strong>Yash Pandey</strong></h1>
-        <h3 className="text-lg font-wix-made-for-display">AND THIS IS MY WEBSITE</h3>
+        <h1 className="text-7xl font-apple-system text-center">Hello, I&#39;m</h1>
+        <h1 className="text-8xl font-apple-system text-center"><strong>Yash Pandey</strong></h1>
+        <h3 className="text-lg font-wix-made-for-display text-center">AND THIS IS MY WEBSITE</h3>
     </div>
     )
 }
