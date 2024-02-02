58fbae6818684e019b315a14856bf4133f244dcc
Yash Pandey <pandeyash06@gmail.com>
2024-02-02 23:20:30 +0530

    animated hamburger icon

--- app/components/Navigation/SideBar/Sidebar.tsx
+++ app/components/Navigation/SideBar/Sidebar.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+
 
 function Sidebar(props: any) {
+  
+
+  
   function HandleClick(l: string) {
     console.log("clicked " + l);
   }
@@ -16,12 +19,21 @@ function Sidebar(props: any) {
 
   return (
     <div>
-      <button
+      
+      <div className="fixed right-0 p-5 inline-flex items-center md:hidden z-50">
+      <label htmlFor="check">
+        <input type="checkbox" checked={props.isOpen} onClick={props.toggle} id="check" />
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+      </div>
+
+      {/* <button
         type="button"
         className="fixed right-0 p-5 inline-flex items-center md:hidden z-top"
         onClick={props.toggle}
       >
-        {/* Menu icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="40"
@@ -30,7 +42,7 @@ function Sidebar(props: any) {
         >
           <path fill="#fff" d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z" />
         </svg>
-      </button>
+      </button> */}
       <div
         className="sidebar-container fixed w-full h-screen overflow-hidden justify-center bg-black grid pt-[120px] left-0 z-10"
         style={{
@@ -38,8 +50,10 @@ function Sidebar(props: any) {
           top: ` ${props.isOpen ? "0" : "-100%"}`,
         }}
       >
-        <button className="absolute right-0 p-5" onClick={props.toggle}>
-          {/* Close icon */}
+        
+
+        {/* <button className="absolute right-0 p-5" onClick={props.toggle}>
+          Close icon
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="48"
@@ -51,7 +65,7 @@ function Sidebar(props: any) {
               d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
             />
           </svg>
-        </button>
+        </button> */}
 
         <ul>
           <li>
